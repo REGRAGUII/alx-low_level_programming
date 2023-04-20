1b0c8232e453c16c363326b23352b4bf8a0c632f
REGRAGUII <youssef98reg@gmail.com>
2023-04-19 18:14:07 -0700

    0x0F-function_pointers

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,13 @@
+#include "function_pointers.h"
+/**
+ *print_name : prints name using a function.
+ * @name: Input name.
+ * @f: A function pointer.
+ * Return: no return.
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	if (name == NULL || f == NULL)
+		return;
+	f(name);
+}
---
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -0,0 +1,27 @@
+#include "3-calc.h"
+
+/**
+ * get_op_func - To select the correct function to do
+ * the operation asked by the user.
+ * @s: Operator type char.
+ *
+ * Return: Pointer to the function corresponding
+ * to the operator given as a parameter.
+ */
+int (*get_op_func(char *s))(int, int)
+{
+	op_t ops[] = {
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL}
+	};
+
+	int a = 0;
+	while (ops[a].op != NULL && *(ops[a].op) != *s)
+		a++;
+
+	return (ops[a].f);
+}
