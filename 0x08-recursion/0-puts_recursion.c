7f25bc8c241552aa276b394e7eac3ff78f9e3ef4
REGRAGUII <youssef98reg@gmail.com>
2023-06-08 16:35:24 -0700

    She locked away a secret, deep inside herself, something she once knew to be true... but chose to forget

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,18 @@
+#indclude "main.h"
+
+/**
+ * _puts_recursion - fills memory with a constant byte.
+ * @s: first bytes of the memory 
+ * Return: -
+ */
+void _puts_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		_putchar('\n');
+		return;
+	}
+	_putchar(*s);
+	s++;
+	_puts_recursion(s);
+}
