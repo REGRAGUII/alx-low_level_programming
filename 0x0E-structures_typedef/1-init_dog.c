68a96612a8b410faea28d237b0d4e0534d08fb21
REGRAGUII <youssef98reg@gmail.com>
2023-06-20 13:26:21 -0700

    C-struct

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,11 +1,12 @@
-#include "dog.h"
 #include <stdio.h>
- /**
- * init_dog - entry
- * @d: size of
- * @name: name of
- * @age: age of
- * @owner: name of the owner
+#include "dog.h"
+
+/**
+ * init_dog - entry point
+ * @d: size of triangle
+ * @name: size of triangle
+ * @age: size of triangle
+ * @owner: size of triangle
  * Description: --
  * Return: --
  */
