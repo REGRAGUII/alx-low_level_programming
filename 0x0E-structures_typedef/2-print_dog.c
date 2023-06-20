ca57b3c23c1b3dc6318bf47c35545612edfa400e
REGRAGUII <youssef98reg@gmail.com>
2023-06-20 13:28:52 -0700

    C-struct

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -7,12 +7,11 @@
  * Description: --
  * Return: --
  */
-
 void print_dog(struct dog *d)
 {
 	if (d == NULL)
 		return;
 	printf("Name: %s\n", d->name ? d->name : "(nil)");
 	printf("Age: %f\n", d->age);
-	printf("owner: %s\n", d->owmer ? d->owmer : "(nil)");
+	printf("Owner: %s\n", d->owner ? d->owner : "(nil)");
 }
